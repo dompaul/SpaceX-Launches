44da183b5881334ec480c8aa97f0a0e2b99c7522
Dominic Paul <dominic.paul@pulselive.com>
2020-11-16 17:05:33 +0000

    Setup App.js file. Start initial markup/templating of app and write out initial functions to be used

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,80 @@
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends React.Component {
+    /**
+     * This app consumes data from the SpaceX public API.
+     * And loads a full list of SpaceX launches, historic and future launches
+     *
+     * @constructor
+     */
+    constructor( props ) {
+        super( props );
+        this.state = {
+            items: [],
+            isLoaded: false,
+            error: false,
+            sort: false,
+            years: [],
+            filter: ''
+        };
+    }
 
-    </div>
-  );
+    /**
+     * Runs after the component output has been rendered to the DOM
+     */
+    componentDidMount() {
+
+    }
+
+    /**
+     * Responsible for getting the SpaceX dataset from the SpaceX API
+     */
+    getLaunches() {
+
+    }
+
+    /**
+     * Responsible for getting the list of years available in the SpaceX API
+     *
+     * @param  {Object} json Response dataset from SpaceX API
+     * @return {Array} Returns an Array of years
+     */
+    getYears( json ) {
+
+    }
+
+    /**
+     * Callback function from onClick event listener
+     * Handles updating the items state with the new, sorted list
+     */
+    handleSort() {
+
+    }
+
+    /**
+     * Callback function from the onChange listener
+     * Handles updating the filter state
+     *
+     * @param  {Object} event Filter on change event (e.g 2009)
+     */
+    handleFilter( event ) {
+
+    }
+
+    render() {
+
+        return (
+            <div className="app">
+                <header className="app__header">
+                    <div className="app__logo">
+
+                    </div>
+                </header>
+
+                <div className="app__body">
+
+                </div>
+            </div>
+        );
+    }
 }
-
-export default App;
